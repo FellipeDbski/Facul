94702c1f57884810401745fb87e3d2d3e0ae284b
Fellipe Dobokowski <dobokowskifellipe@gmail.com>
2018-04-08 15:24:57 -0300

    Exercicios corrigidos, todas as funções do projeto 'Aula c' estão funcionando perfeitamente, respeitando as boas práticas.

--- AulasC/main.c
+++ AulasC/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 struct Pessoa {
-	char Nome;
-	char RG;
+	char Nome[30];
+	char RG[30];
 	int Telefone;
 };
 
@@ -11,70 +12,67 @@ int p = 0;
 struct Pessoa dados[100];
 
 
-int Cadastro()
+int cadastro()
 {
-	char nome;
-	char rg;
-	int telefone;
-
+    printf("-------------------------------------------------------------- \n");
 	printf("Realizando novo cadastro \n");
 	printf("-------------------------------------------------------------- \n");
+
 	printf("Digite seu nome: ");
-	scanf("%c", nome);
-	dados[p].Nome = nome;
+	scanf("%s", &dados[p].Nome);
 
 	printf("Digite seu rg: ");
-	scanf("%c", rg);
-	dados[p].RG = rg;
-
+	scanf("%s", &dados[p].RG);
 
 	printf("Digite seu telefone: ");
-	scanf("%i", telefone);
-	dados[p].Telefone = telefone;
+	scanf("%d", &dados[p].Telefone);
+    p++;
+    printf("-------------------------------------------------------------- \n");
+    menu();
 
-	printf("-------------------------------------------------------------- \n");
-	printf("Cadastro Realizado com sucesso");
-	p++;
+
 	return 0;
 }
 
-int Altera(){
+int altera(){
 	int codigo;
 	printf("-------------------------------------------------------------- \n");
 	printf("Digite o código da pessoa cadastrada \n");
 	printf("-------------------------------------------------------------- \n");
 	printf("Código:  ");
-	scanf("%i", codigo);
+	scanf(" %d", &codigo);
 	if (codigo >= 0 && codigo <= 100) {
 		printf("-------------------------------------------------------------- \n");
-		printf("Realizando uma alteração no cadastro \n");
-		scanf("%c", dados[codigo].Nome);
-		scanf("%c", dados[codigo].RG);
-		scanf("%i", dados[codigo].Telefone);
+		printf("Realizando uma ção no cadastro \n");
+		scanf(" %c", &dados[codigo].Nome);
+		scanf(" %c", &dados[codigo].RG);
+		scanf(" %d", &dados[codigo].Telefone);
 		printf("-------------------------------------------------------------- \n");
-		printf("Alteração realizada com sucesso \n");
+		printf("Ação realizada com sucesso \n");
 	}
-	return 0;
+	return main();
 }
 
-int Exibe() {
-	for (int x = 0; x <= p; x++)
+int exibe() {
+	for (int x = 0; x < p; x++)
 	{
+		printf("--------------------------------------------------------------\n");
+		printf("Exibindo Usuários Cadastrados em Ordem \n");
 		printf("-------------------------------------------------------------- \n");
-		printf("Exibindo Usuários Cadastrados em Ordem");
-		printf("-------------------------------------------------------------- \n");
-		printf("PESSOA : \n");
-         printf(dados[x].Nome);
-		 printf(dados[x].RG);
-		 printf(dados[x].Telefone);
+		printf("CADASTRO Nº%d", x);
+         printf("NOME: %s\n", dados[x].Nome);
+		 printf("RG: %s\n", dados[x].RG);
+		 printf("TELEFONE: %d\n", dados[x].Telefone);
 		printf("-------------------------------------------------------------- \n");
 	}
-
-	return 0;
+	return main();
 }
 
-int Menu() {
+int menu() {
 	int i;
+	printf("-------------------------------------------------------------- \n");
+    printf("Limite de cadastros: %d", p);
+    printf("/100 \n");
 	printf("-------------------------------------------------------------- \n");
 	printf("Digite [1] para cadastrar uma nova pessoa \n");
 	printf("Digite [2] para alterar um cadastro \n");
@@ -82,17 +80,28 @@ int Menu() {
 	printf("Digite [4] para sair: \n");
 	printf("-------------------------------------------------------------- \n");
 	printf("Digite:  ");
-	scanf("%i", i);
+	scanf("%d", &i);
+	printf("-------------------------------------------------------------- \n");
+	if(i == 1){
+        return cadastro();
+	};
+	if(i == 2){
+        return altera();
+	};
+	if(i == 3){
+        return exibe();
+	}
+	else{
+        return 0;
+	};
+}
 
-
-
-int main()
+int main(void)
 {
+    setlocale(LC_ALL, "Portuguese");
 	printf("Bem vindo ao cadastro de pessoas \n");
 	printf("-------------------------------------------------------------- \n");
-
-	Menu();
+	menu();
 	printf("-------------------------------------------------------------- \n");
-
-	return 0;
+	return (0);
 }
